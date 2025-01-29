39098773a9047e14ef4e0c59a1558ab8f3b68ebd
Almaro20 <amr0100@alu.medac.es>
2025-01-29 17:28:25 +0100

    prueba

---
+++ resources/js/react/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Principal from './Principal.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <>
+    
+        <Principal />
+
+    </>
+   
+  
+)

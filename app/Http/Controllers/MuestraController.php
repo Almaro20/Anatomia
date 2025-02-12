92b8d428b874b3d0dcade1975a0ec7a0ae0e1479
diegojura <154268497+diegojura@users.noreply.github.com>
2025-02-12 20:07:58 +0100

    api antiguo nuevo

--- app/Http/Controllers/MuestraController.php
+++ app/Http/Controllers/MuestraController.php
@@ -7,29 +7,16 @@ use App\Models\Muestra;
 class MuestraController extends Controller
 {
     public function getAll() {
-        return response()->json(Muestra::with('tipoNaturaleza', 'organo', 'formato', 'calidad', 'sede', 'user')->get(), 200);
+        return response()->json(Muestra::all(), 200);
     }
 
     public function create(Request $request) {
-        $muestra = new Muestra();
-        $muestra->codigo = $request->codigo;
-        $muestra->fechaEntrada = $request->fechaEntrada;
-        $muestra->organo = $request->organo;
-        $muestra->descripcionMuestra = $request->descripcionMuestra;
-        $muestra->tipoNaturaleza_id = $request->tipoNaturaleza_id;
-        $muestra->formato_id = $request->formato_id;
-        $muestra->calidad_id = $request->calidad_id;
-        $muestra->sede_id = $request->sede_id;
-        $muestra->user_id = $request->user_id;
-        if ($muestra->save()) {
-            return response()->json($muestra, 201);
-        } else {
-            return response()->json(['message' => 'No se ha podido crear la muestra'], 400);
-        }
+        $muestra = Muestra::create($request->all());
+        return response()->json($muestra, 201);
     }
 
     public function getById($id) {
-        $muestra = Muestra::with('tipoNaturaleza', 'organo', 'formato', 'calidad', 'sede', 'user')->find($id);
+        $muestra = Muestra::find($id);
         if (!$muestra) {
             return response()->json(['message' => 'Muestra no encontrada'], 404);
         }
@@ -41,11 +28,8 @@ class MuestraController extends Controller
         if (!$muestra) {
             return response()->json(['message' => 'Muestra no encontrada'], 404);
         }
-        if ($muestra->update($request->all())) {
-            return response()->json($muestra, 200);
-        } else {
-            return response()->json(['message' => 'No se ha podido actualizar la muestra'], 400);
-        }
+        $muestra->update($request->all());
+        return response()->json($muestra, 200);
     }
 
     public function delete($id) {
@@ -53,11 +37,7 @@ class MuestraController extends Controller
         if (!$muestra) {
             return response()->json(['message' => 'Muestra no encontrada'], 404);
         }
-        if ($muestra->delete()) {
-            return response()->json(['message' => 'Muestra eliminada'], 200);
-        } else {
-            return response()->json(['message' => 'No se ha podido eliminar la muestra'], 400);
-        }
+        $muestra->delete();
+        return response()->json(['message' => 'Muestra eliminada'], 200);
     }
 }
-

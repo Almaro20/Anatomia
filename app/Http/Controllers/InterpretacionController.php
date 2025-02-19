601a7841571d0322d531b9448f1f8ff852cd940c
diegojura <154268497+diegojura@users.noreply.github.com>
2025-02-19 18:47:33 +0100

    si eso

--- app/Http/Controllers/InterpretacionController.php
+++ app/Http/Controllers/InterpretacionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Interpretacion;
-use Illuminate\Http\Request;
 
 class InterpretacionController extends Controller
 {
@@ -15,7 +14,7 @@ class InterpretacionController extends Controller
 
     public function getByCodigo($codigo)
     {
-        $interpretaciones = Interpretacion::where('codigo', 'like', "$codigo.%") // Busca con el número antes del punto
+        $interpretaciones = Interpretacion::where('codigo', 'like', "$codigo.%")
             ->get();
 
         return response()->json($interpretaciones, 200);
